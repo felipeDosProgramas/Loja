baa459e490135b082db467618594e8d510b0e5e1
FelipeDosProgramas <felipeluizmsouza@gmail.com>
2023-07-16 21:33:39 -0300

    tela de produto individual
    agora, o botão do  tamanho especificado tem uma classe especifica para se estilizar.
    
    peço que, no front, faça uma div com botões que irão receber cores em hexadecimal do back. Com eles, o dono terá a opção de salvar fotos do produto com variações de cor.
    
    depois discutiremos sobre isso :+1:

--- js/dadosProdutoParser.js
+++ js/dadosProdutoParser.js
@@ -1,12 +1,11 @@
-let getDaUrl = window.location.href.split('?');
-getDaUrl = getDaUrl[1].split('=');
-getDaUrl = getDaUrl[1];
-
+let getDaUrl = window.location.href.split('?');getDaUrl = getDaUrl[1].split('=');getDaUrl = getDaUrl[1];
 let Nome = document.getElementById('nameAnun');
 let Preco = document.getElementById('divPreco');
 let descricao = document.getElementById('description');
 let putInSaco = document.getElementById('btnSaco');
-
+let divTamanhos = document.getElementById('divSize');
+let divPreco = document.getElementById('divPreco');
+let todosTamanhos = document.getElementsByClassName('sizeBtn');
 class putInFront{
 	
 	constructor(tudo){
@@ -14,35 +13,57 @@ class putInFront{
 	}
 	setTextos(){
 		Nome.innerHTML = this.tudo.nome;
-		Preco.innerHTML = this.tudo.preco[0];
 		descricao.innerHTML = this.tudo.descricao;
 	}
 	
 	setImagens(){
 		let fotos = [];
 		this.tudo.fotos.forEach((cada)=>{
-		fotos.push("imgs/Produtos/"+getDaUrl+"/"+cada);
+			fotos.push("imgs/Produtos/"+getDaUrl+"/"+cada);
 		});
 		sessionStorage.setItem('imgs', JSON.stringify(fotos));
 	}
-	
+	selecionaEsse(qual){
+		let qnts = todosTamanhos.length;
+		for(let x = 0;x != qnts;x++){
+			todosTamanhos[x].className = 'sizeBtn'
+		}
+		todosTamanhos[qual].className += ' selectedSizeOption'
+	}
+	setPrecosETamanhos(){
+		let sizes = [];
+		let aux = 0;				
+		divTamanhos.innerText = "";
+		this.tudo.Tamanho.forEach((cada)=>{			
+			let aq = aux; /* deixe isso aqui dessa maneira, se colocar na variavel aux diretamente, ele recuperará o último valor de aux toda vez que for clicado o botão*/
+			let btn = document.createElement('button');
+			btn.className = 'sizeBtn';
+			btn.addEventListener('click',() => this.selecionaEsse(aq))
+			btn.innerText = this.tudo.Tamanho[aq];
+			divTamanhos.append(btn);
+			aux++;
+		})
+		
+		todosTamanhos[0].className += ' selectedSizeOption'
+	}
 }
 
 (async () => {
 	let response = await fetch("purePhp/produtos.php?oq=especifico&qual="+getDaUrl);
 	let tudo = await response.text();		
+	console.log(tudo)
 	tudo = JSON.parse(tudo);	
 	tudo = new putInFront(tudo);
 	tudo.setTextos();
 	tudo.setImagens();
-	// console.log(tudo)
+	tudo.setPrecosETamanhos();
+	
 })();
 
 
 putInSaco.addEventListener('click', async ()=>{
-	
-	let addEle = await fetch("purePhp/TinyCar.php?oq=addSoEsse&qual="+getDaUrl);
-	let foiOuNn = await addEle.text();
-	
-	console.log(foiOuNn)
+
+let addEle = await fetch("purePhp/TinyCar.php?oq=addSoEsse&qual="+getDaUrl);
+let foiOuNn = await addEle.text();
+console.log(foiOuNn)
 })
