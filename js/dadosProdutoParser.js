161225a175763c8b597e56f76ee60c854a4d4fb3
FelipeDosProgramas <felipeluizmsouza@gmail.com>
2023-07-16 23:07:10 -0300

    URL
    mudança em como o js interpreta a URL

--- js/dadosProdutoParser.js
+++ js/dadosProdutoParser.js
@@ -1,4 +1,4 @@
-let getDaUrl = window.location.href.split('?');getDaUrl = getDaUrl[1].split('=');getDaUrl = getDaUrl[1];
+let getDaUrl = window.location.href;
 let Nome = document.getElementById('nameAnun');
 let Preco = document.getElementById('divPreco');
 let descricao = document.getElementById('description');
@@ -6,10 +6,24 @@ let putInSaco = document.getElementById('btnSaco');
 let divTamanhos = document.getElementById('divSize');
 let divPreco = document.getElementById('divPreco');
 let todosTamanhos = document.getElementsByClassName('sizeBtn');
+
+(() =>{
+	
+if(getDaUrl.includes("?")){
+	getDaUrl = getDaUrl.split('?');getDaUrl = getDaUrl[1].split('=');getDaUrl = getDaUrl[1];
+	return;
+}
+getDaUrl = "nao tem";
+})();
 class putInFront{
 	
 	constructor(tudo){
 		this.tudo = tudo;
+		if(this.tudo != "sem dados"){
+			this.setTextos();
+			this.setImagens();
+			this.setPrecosETamanhos();
+		}
 	}
 	setTextos(){
 		Nome.innerHTML = this.tudo.nome;
@@ -26,44 +40,43 @@ class putInFront{
 	selecionaEsse(qual){
 		let qnts = todosTamanhos.length;
 		for(let x = 0;x != qnts;x++){
-			todosTamanhos[x].className = 'sizeBtn'
+			todosTamanhos[x].className = 'sizeBtn';
 		}
-		todosTamanhos[qual].className += ' selectedSizeOption'
+		todosTamanhos[qual].className += ' selectedSizeOption';
+		divPreco.innerText = `R$ ${this.tudo.Preco[qual]}`;
 	}
 	setPrecosETamanhos(){
 		let sizes = [];
 		let aux = 0;				
 		divTamanhos.innerText = "";
+		
 		this.tudo.Tamanho.forEach((cada)=>{			
 			let aq = aux; /* deixe isso aqui dessa maneira, se colocar na variavel aux diretamente, ele recuperará o último valor de aux toda vez que for clicado o botão*/
 			let btn = document.createElement('button');
 			btn.className = 'sizeBtn';
 			btn.addEventListener('click',() => this.selecionaEsse(aq))
-			btn.innerText = this.tudo.Tamanho[aq];
+			btn.innerText = this.tudo.Tamanho[aq];			
 			divTamanhos.append(btn);
 			aux++;
 		})
 		
-		todosTamanhos[0].className += ' selectedSizeOption'
+		todosTamanhos[0].className += ' selectedSizeOption';
+		divPreco.innerText = `R$ ${this.tudo.Preco[0]}`;
 	}
 }
 
 (async () => {
 	let response = await fetch("purePhp/produtos.php?oq=especifico&qual="+getDaUrl);
-	let tudo = await response.text();		
-	console.log(tudo)
-	tudo = JSON.parse(tudo);	
-	tudo = new putInFront(tudo);
-	tudo.setTextos();
-	tudo.setImagens();
-	tudo.setPrecosETamanhos();
-	
+let tudo = await response.text();		
+console.log(tudo)
+tudo = JSON.parse(tudo);	
+tudo = new putInFront(tudo);	
 })();
 
 
 putInSaco.addEventListener('click', async ()=>{
-
-let addEle = await fetch("purePhp/TinyCar.php?oq=addSoEsse&qual="+getDaUrl);
-let foiOuNn = await addEle.text();
-console.log(foiOuNn)
+	
+	let addEle = await fetch("purePhp/TinyCar.php?oq=addSoEsse&qual="+getDaUrl);
+	let foiOuNn = await addEle.text();
+	console.log(foiOuNn)
 })
