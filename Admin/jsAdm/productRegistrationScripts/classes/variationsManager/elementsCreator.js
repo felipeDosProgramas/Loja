5be1999acdd4013bdd21bbda60aa6c34f7e7727b
FelipeDosProgramas <felipeluizmsouza@gmail.com>
2023-08-19 07:19:13 -0300

    agora 100% operante
    a atualização EM TEMPO REAL da criação de opções de tamanhos e cores

--- Admin/jsAdm/productRegistrationScripts/classes/variationsManager/elementsCreator.js
+++ Admin/jsAdm/productRegistrationScripts/classes/variationsManager/elementsCreator.js
@@ -51,19 +51,33 @@ class elementsCreator{
 		console.log(opts)
 		return opts
 	}
-	refreshSizesOptions(){
-		console.clear()
+	cleanSelectChilds(select){
+		while(select.firstChild){
+			select.firstChild.remove()
+		}
+		return select
+	}
+	createSizeOptions(){
 		let tamanhos = this.inputSizes.value.split(',') 
 		tamanhos.forEach((cada, um) => tamanhos[um] = cada.trim())
 		
 		tamanhos = this.createOptions(tamanhos)
 		
-		this.selectsSizes.forEach((cada)=>{
+		return tamanhos
+	}
+	refreshSizesOptions(){
+		console.clear()		
+		
+		
+		this.selectsSizes.forEach((cada)=>{			
+			const tamanhos = this.createSizeOptions()
+			
+			cada = this.cleanSelectChilds(cada)
+			
 			tamanhos.forEach((opt) => {
 				cada.append(opt)
 			})
 		})
-		console.log(tamanhos)		
 	}	
 	
 	generateSizeSelect(){
