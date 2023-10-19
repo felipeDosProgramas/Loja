d8d624285dbd42095c3bd3f6d4f772e22c95c172
Caio dos Santos Oliveira <caiossantos4321@gmail.com>
2023-10-19 15:04:43 -0300

    video

--- js/video.js
+++ js/video.js
@@ -1,4 +1,4 @@
 setTimeout(function() {
 	document.getElementById('entre').style.display = 'block'; // Ou 'inline' ou 'inline-block' dependendo do seu layout
 	document.getElementById('video').style.display = 'none'
-}, 5000);
+}, 13000);
